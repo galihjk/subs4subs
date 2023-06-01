de26155b096f5299795507f4552610c30c6cb091
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-06-02 02:46:19 +0700

    debug1

--- functions/check_sbp_habis.php
+++ functions/check_sbp_habis.php
@@ -7,6 +7,10 @@ function check_sbp_habis($userid){
         foreach($userchannels as $channel){
             $output_text .= "- $channel\n";
             $msgid = f("data_load")("channelposts/$userid-$channel");
+            if(empty($msgid)){
+                $output_text .= "ERROR channelposts/$userid-$channel is empty\n";
+                continue;
+            }
             $deleteMsg = f("bot_kirim_perintah")('deleteMessage',[
                 'chat_id' => f("get_config")("s4s_channel"),
                 'message_id' => $msgid,
