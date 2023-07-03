1c38e5c70e3b680241d39610f3e5b23204b38eb5
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-07-03 12:46:24 +0700

    sudah

--- job_run.php
+++ job_run.php
@@ -9,7 +9,9 @@ if($last_job_run != $current_time){
     echo "<pre>";
     echo "current_time=$current_time - last_job_run=$last_job_run ==== ".($current_time-$last_job_run)."\n";
     
-    echo f("check_unsubscribe_user")();
+    $hasil_job = f("check_unsubscribe_user")();
+    echo $hasil_job;
+    file_put_contents("log/LAST_JOB_".date("Y-m").".txt",$hasil_job);
     
 }
 else{
