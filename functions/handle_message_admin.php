30336bedf2b1eb15c221887caa8e8e4d3e670db7
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-07-04 09:17:46 +0700

    add_sbp

--- functions/handle_message_admin.php
+++ functions/handle_message_admin.php
@@ -16,23 +16,47 @@ function handle_message_admin($botdata){
     if($is_admin and $text == "/admin"){
         $outputtext = "/users \n";
         $outputtext .= "/u_{id} \n";
-        $outputtext .= "/unban_user_{id} \n";
-        $outputtext .= "/unban_channel_{channel} \n";
+        $outputtext .= "/add_sbp (spasi) {id} (spasi) {tambahan sbp}\n";
+        $outputtext .= "/unban_user (spasi) {id} \n";
+        $outputtext .= "/unban_channel (spasi) {channel} \n";
         f("bot_kirim_perintah")("sendMessage",[
             "chat_id"=>$botdata["chat"]["id"],
             "text"=>$outputtext,
         ]);
     }
-    elseif($is_admin and f("str_is_diawali")($text,"/unban_user_")){
-        $unbanuser = str_replace("@".f("get_config")("botuname"),"",str_replace("/unban_user_","",$text));
+    elseif($is_admin and f("str_is_diawali")($text,"/add_sbp ")){
+        $add_sbp = str_replace("@".f("get_config")("botuname"),"",str_replace("/add_sbp ","",$text));
+        $explode = explode(" ",$add_sbp);
+        if(!empty($explode[0]) and !empty($explode[1])){
+            $add_user = $explode[0];
+            $add_value = $explode[1];
+            $user_sbp = f("get_usersbp")($add_user);
+            $user_sbp_awal = $user_sbp;
+            $user_sbp += $add_value;
+            f("set_usersbp")($add_user,$user_sbp);
+
+            f("bot_kirim_perintah")("sendMessage",[
+                "chat_id"=>$botdata["chat"]["id"],
+                "text"=>"SBP /u_$add_user diubah dari $user_sbp_awal menjadi $user_sbp oleh /u_$userid",
+            ]);
+        }
+        else{
+            f("bot_kirim_perintah")("sendMessage",[
+                "chat_id"=>$botdata["chat"]["id"],
+                "text"=>"👎",
+            ]);
+        }
+    }
+    elseif($is_admin and f("str_is_diawali")($text,"/unban_user ")){
+        $unbanuser = str_replace("@".f("get_config")("botuname"),"",str_replace("/unban_user ","",$text));
         unlink("data/banned_users/$unbanuser");
         f("bot_kirim_perintah")("sendMessage",[
             "chat_id"=>$botdata["chat"]["id"],
             "text"=>"👍",
         ]);
     }
-    elseif($is_admin and f("str_is_diawali")($text,"/unban_channel_")){
-        $unban_channel = str_replace("@".f("get_config")("botuname"),"",str_replace("/unban_channel_","",$text));
+    elseif($is_admin and f("str_is_diawali")($text,"/unban_channel ")){
+        $unban_channel = str_replace("@".f("get_config")("botuname"),"",str_replace("/unban_channel ","",$text));
         unlink("data/banned_channels/$unban_channel");
         f("bot_kirim_perintah")("sendMessage",[
             "chat_id"=>$botdata["chat"]["id"],
